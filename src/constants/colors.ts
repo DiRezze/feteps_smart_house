ef6e0cee78532bbefea568f3eca1367ee94c9b92
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2025-01-03 07:54:44 -0300

    created: constant/colors.ts

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+    light: {
+        primary: '#10CE40',
+        primaryOutline: '#00741D',
+        softBlack: '#060606',
+        softGray: '#666666',
+        softWhite: '#FEFEFE',
+        background: 'FEFEFE'
+    },
+    dark: {
+        primary: '#10CE40',
+        primaryOutline: '#00741D',
+        softBlack: '#060606',
+        softGray: '#666666',
+        softWhite: '#FEFEFE',
+        background: '#1E1E1E'
+    }
+};
